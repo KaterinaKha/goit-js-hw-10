75c54d37637c75df826e735e81fbf440a5ae9824
KaterinaKha <katerinakhaa@gmail.com>
2023-05-16 02:26:25 +0200

    delete console.log

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ function inputOnSearchBox(e) {
   } else if (!!inputValue) {
     fetchCountries(inputValue)
       .then(countries => {
-        console.log(countries);
         if (countries.length > 10) {
           Notify.info(
             'Too many matches found. Please enter a more specific name.'
@@ -32,7 +31,6 @@ function inputOnSearchBox(e) {
           );
           refs.countryInfoEl.innerHTML = markup;
           refs.countryListEl.innerHTML = '';
-          console.log(countries);
         } else if (countries.length === 1) {
           refs.countryListEl.innerHTML = getCountryListMarkup(countries[0]);
         }
